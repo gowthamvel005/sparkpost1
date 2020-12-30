5a241451bc4aed3e66eebb24740ccbb12981547b
BalajiRavindran30 <73240976+BalajiRavindran30@users.noreply.github.com>
2020-12-30 13:06:19 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -103,15 +103,15 @@ define([
         if (currentStep.key === 'step3' || currentStep.key === 'step2') {
             save();
         } else {
-            
-        var input = $('#text-input-id-1')[0];
-	console.log('input value '+input);
-	var validityState_object = input.validity;
-	if (validityState_object.valueMissing){
-	    input.setCustomValidity('Must enter your template name!');
-	    input.reportValidity();
-	}
-        connection.trigger('nextStep');
+		var input = $('#text-input-id-1')[0];
+		console.log('input value '+input);
+		var validityState_object = input.validity;
+		if (validityState_object.valueMissing){
+	    		input.setCustomValidity('Must enter your template name!');
+	    		input.reportValidity();
+		} else {
+	    		connection.trigger('nextStep');
+		}
         }
     }
 
