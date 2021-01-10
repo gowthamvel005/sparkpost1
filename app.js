6f25986b015dc045b70f1bc10504c9962d3e0ba7
BalajiRavindran30 <73240976+BalajiRavindran30@users.noreply.github.com>
2021-01-10 22:32:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/json'}));
 
 //app.use(express.methodOverride());
 //app.use(express.favicon());
