ee01f7e486716045c7fd680b0db4dc6a08e228d1
SunMartekVentures <76422802+SunMartekVentures@users.noreply.github.com>
2021-01-29 00:13:58 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -486,6 +486,8 @@ exports.insertDERow = function (req, res) {
     	
 	console.log( 'insert data '+JSON.stringify(req.body));
 	//console.log( 'insert body data '+[req.body.xmlData]);
+	var insData = JSON.stringify([{"keys":req.body.xmlData.keys,"values":req.body.xmlData.values}]);
+	console.log( 'insert body insData '+ JSON.stringify(insData));
 	var config = {
 	    method: 'post',
             url: 'https://'+process.env.mcEndpoint+'.rest.marketingcloudapis.com/hub/v1/dataevents/key:Data_Extension_Template/rowset',
@@ -493,7 +495,7 @@ exports.insertDERow = function (req, res) {
 		    'Content-Type': 'application/json',
 		    'Authorization': 'Bearer '+req.body.token
             },
-            data : req.body.xmlData
+            data : insData
     	};
 	
 	axios(config)
